bba2d2ed55aab9323a53410065f79c461bcad6d5
fatimalaghari13 <fatimalaghari13@gmail.com>
2018-07-24 23:50:19 +0000

    automatically backed up by learn

---
+++ until.rb
@@ -0,0 +1,11 @@
+def wingardium_leviosa
+  levitation=6
+  
+  until levitation==10
+    puts "wingardium_leviosa"
+    puts ""
+    levitation+=1
+  end
+end
+
+wingardium_leviosa
---
+++ while.rb
@@ -0,0 +1,10 @@
+def wingardium_leviosa
+  levitation=6
+  while levitation <=10
+    puts "wingardium_leviosa"
+    puts ""
+  levitation+=1
+  end
+end
+
+wingardium_leviosa
